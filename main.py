c18b224733d6e8a417bc33cc8db7b602c8358161
Rafael Rosal <66257351+RafaelRosal@users.noreply.github.com>
2023-04-04 16:13:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import threading
+
+
+def menu():
+    print('---CINEMA---')
+    print('Informe uma das opções a seguir: ')
+    print('\n 1 - Reservar assento\n', '2 - Cancelar reserva\n', '0 - Encerrar')
+
+
+print(menu())
